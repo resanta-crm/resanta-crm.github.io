b5aa670863215e10efe8c5b8c60def5edf83c464
resanta-crm <resantavitebsc@gmail.com>
2026-09-21 18:53:19 +0300

    TSD v23.6.145: refresh hardware scanner diagnostics

--- inventory-sw.js
+++ inventory-sw.js
@@ -1,4 +1,4 @@
-const CACHE='resanta-inventory-v236144';
+const CACHE='resanta-inventory-v236145';
 const STATIC=['/inventory-manifest.webmanifest','/inventory-icon.svg','/inventory-qr.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
