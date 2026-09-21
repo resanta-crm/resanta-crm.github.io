27c4dc4b7713ea3fef13551b73afb1b8126eeb97
resanta-crm <resantavitebsc@gmail.com>
2026-09-21 16:47:59 +0300

    Add inventory PWA service worker

---
+++ inventory-sw.js
@@ -0,0 +1,10 @@
+const CACHE='resanta-inventory-v236139';
+const STATIC=['/inventory.html','/inventory-manifest.webmanifest','/inventory-icon.svg','/inventory-qr.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)));
+});
